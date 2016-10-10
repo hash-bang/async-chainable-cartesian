9a763b7010c971761faf523d7027475b194b12f1
Matt Carter <m@ttcarter.com>
2016-10-10 15:36:32 +1000

    Basic tests now passing

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-function lazyProduct(sets,f,context){
-	if (!context) context=this;
-	var p=[],max=sets.length-1,lens=[];
-	for (var i=sets.length;i--;) lens[i]=sets[i].length;
-	function dive(d){
-		var a=sets[d], len=lens[d];
-		if (d==max) for (var i=0;i<len;++i) p[d]=a[i], f.apply(context,p);
-		else        for (var i=0;i<len;++i) p[d]=a[i], dive(d+1);
-		p.pop();
-	}
-	dive(0);
-}
-
-
 module.exports = function() {
-	// product() {{{
-	this._plugins['product'] = function(params) {
-		lazyProduct(params.content, function(i) {
-			params.callback.apply(this, i);
-		});
-
-		this._execute();
-	};
-
 	this.product = function() {
+		var sets = arguments[0];
+		var maxLength;
+		for (var dm=[], maxLength=1 ,l ,i = sets.length; i--; maxLength *= l){
+			dm[i] = [maxLength, l = sets[i].length];
+		}
+
 		this._struct.push({
-			type: 'product',
-			content: arguments[0,
+			type: 'forEachRange',
+			min: 0,
+			max: maxLength - 1,
 			callback: arguments[1],
+			translate: function(v, n, max) {
+
+				for (var c = [], i = sets.length; i--;) {
+					c[i] = sets[i][(n / dm[i][0] << 0) % dm[i][1]];
+				}
+
+				return c;
+			},
 		});
 
 		return this;
 	};
-	// }}}
 };
--- test/basic.js
+++ test/basic.js
@@ -1,4 +1,5 @@
 var async = require('async-chainable');
+var asyncCartesian = require('..');
 var expect = require('chai').expect;
 
 describe('Basic test', function() {
@@ -7,34 +8,35 @@ describe('Basic test', function() {
 	var length;
 
 	before(function(done) {
-		async()
-			.set('a', ['foo', 'bar', 'baz'])
-			.set('b', ['alpha', 'beta', 'gamma'])
+		var a = ['foo', 'bar', 'baz'];
+		var b = ['alpha', 'beta', 'gamma'];
 
+		async()
+			.use(asyncCartesian)
 			.product([a, b], function(next, value, index, max) {
 				if (length === undefined) length = max;
-				vals.push({value: value, index: index, max: max});
+				vals.push({v: value, i: index, m: max});
 				next();
 			})
 
-			.end();
+			.end(done);
 	});
 
 	it('should have calculated the length correctly', function() {
-		expect(length).to.be.equal(8);
+		expect(length).to.be.equal(9);
 	});
 
 	it('should have calculated the product correctly', function() {
 		expect(vals).to.deep.equal([
-			['foo', 'alpha'],
-			['foo', 'bar'],
-			['foo', 'baz'],
-			['bar', 'alpha'],
-			['bar', 'beta'],
-			['bar', 'gamma'],
-			['baz', 'alpha'],
-			['baz', 'beta'],
-			['baz', 'gamma'],
+			{v: ['foo', 'alpha'], i: 0, m: 9},
+			{v: ['foo', 'beta'], i: 1, m: 9},
+			{v: ['foo', 'gamma'], i: 2, m: 9},
+			{v: ['bar', 'alpha'], i: 3, m: 9},
+			{v: ['bar', 'beta'], i: 4, m: 9},
+			{v: ['bar', 'gamma'], i: 5, m: 9},
+			{v: ['baz', 'alpha'], i: 6, m: 9},
+			{v: ['baz', 'beta'], i: 7, m: 9},
+			{v: ['baz', 'gamma'], i: 8, m: 9},
 		]);
 	});
 });
